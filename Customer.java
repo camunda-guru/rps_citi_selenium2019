4aac20a32ac559865fbc7c02011cb6d750bb4036
eswaribala <Parameswaribala@gmail.com>
2019-04-09 14:54:10 +0530

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,43 @@
+package com.citi.banking.entities;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+//creating customer class
+public class Customer {
+
+	//data members
+	private String firstName;
+	private String lastName;
+	private long salary;
+	private LocalDate dob;
+	
+	
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public long getSalary() {
+		return salary;
+	}
+	public void setSalary(long salary) {
+		this.salary = salary;
+	}
+	public LocalDate getDob() {
+		return dob;
+	}
+	public void setDob(LocalDate dob) {
+		this.dob = dob;
+	}
+	
+	
+}
