e00e6bc83747e8c13414c5f0180f8fe781c91c59
eswaribala <Parameswaribala@gmail.com>
2019-04-09 15:18:23 +0530

    Add files via upload

--- CustomerApp.java
+++ CustomerApp.java
@@ -9,44 +9,63 @@ import java.util.Scanner;
 import com.citi.banking.entities.Customer;
 
 class CustomerApp {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//created customer object
-		
-		Scanner scanner = new Scanner(System.in);		
-		
-		Customer customer =new Customer();
-		//set the values
-		System.out.println("Enter First Name");		
+	
+   public static Customer assignvalues(Customer customer)
+   {
+	   Scanner scanner = new Scanner(System.in);
+	   System.out.println("Enter First Name");		
 		customer.setFirstName(scanner.nextLine());
 		System.out.println("Enter Last Name");
 		customer.setLastName(scanner.nextLine());
 		System.out.println("Enter Salary");
 		customer.setSalary(scanner.nextLong());
 		scanner.nextLine();
-		System.out.println("Enter Date of birth(dd-MMM-yyyy)");
-		String dob = scanner.nextLine();
-		//convert string to date
-		SimpleDateFormat ft = new SimpleDateFormat("dd-MMM-yyyy");
-		Date dateobj=null;
-		try {
-			dateobj=ft.parse(dob);
-			customer.setDob(LocalDate.of(dateobj.getYear(),dateobj.getMonth(),dateobj.getDay()));
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		
-		System.out.println("Welcome to Selenium Training");
-		
-		//read the values
-		
-		System.out.println("First Name=\t"+customer.getFirstName());
+		System.out.println("Enter year");
+		int year=scanner.nextInt();
+		scanner.nextLine();
+		System.out.println("Enter month");
+		int month=scanner.nextInt();
+		scanner.nextLine();
+		System.out.println("Enter day");
+		int day=scanner.nextInt();
+		scanner.nextLine();		
+		customer.setDob(LocalDate.of(year, month, day));
+		return customer;
+   }
+	
+   public static void readvalues(Customer customer)
+   {
+	   System.out.println("First Name=\t"+customer.getFirstName());
 		System.out.println("Last Name=\t"+customer.getLastName());
 		System.out.println("Salary=\t"+customer.getSalary());
 		System.out.println("DOB=\t"+customer.getDob().toString());
+	   
+   }
+	
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//created customer object	
+		//Array
+		Scanner scanner =new Scanner(System.in);
+		System.out.println("Enter Customer Count");
+		int count=scanner.nextInt();
+		scanner.nextLine();
+		Customer[] customerArray =new Customer[count];					
+		//set the values
+		for(int i=0;i<count;i++)
+		{
+		    customerArray[i]=new Customer();
+		    customerArray[i]=assignvalues(customerArray[i]); 	
+		}	
+		//read the values
+		//enhanced for loop
+		
+		for(Customer obj : customerArray)
+		 readvalues(obj);
+		
 		
 
 	}
