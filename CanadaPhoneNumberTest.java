4c64f3b4ca289983040ce625029c9f3e54a8df92
eswaribala <Parameswaribala@gmail.com>
2019-04-11 12:51:30 +0530

    Add files via upload

--- CanadaPhoneNumberTest.java
+++ CanadaPhoneNumberTest.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellType;
@@ -14,10 +15,15 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 public class CanadaPhoneNumberTest {
 	private static final String FILE_NAME = "E:\\AAPractise\\Input\\Canada411Input.xlsx";
 	
-	public List<String> getPhoneNumbers()
+	public static List<String> getPhoneNumbers()
 	{
 		 List<String> phoneNumberList=new ArrayList<String>();
 		try {
@@ -62,10 +68,51 @@ public class CanadaPhoneNumberTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
-		
-		
-		
+	   //List<String> phoneList=getPhoneNumbers();
+	   //System.out.println(phoneList.get(0));
+	   //System.setProperty("webdriver.ie.driver","E:\\software\\A08\\file\\IEDriverServer_x64_3.14.0\\IEDriverServer.exe");
+	   System.setProperty("webdriver.chrome.driver","E:\\software\\A08\\file\\chromedriver_win32\\chromedriver.exe");
+	   WebDriver driver=new ChromeDriver();
+   	   driver.get("http://www.canada411.ca/");
+   	   driver.manage().window().maximize();
+   	   driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+   	   WebElement element=null;
+   	   
+   	   //for(String phoneNumber :getPhoneNumbers())
+   	   //{
+   		   element=driver.findElement(By.id("c411PeopleReverseWhat"));
+   		  element.clear();
+   		   element.sendKeys(getPhoneNumbers().get(0));
+   		   element =driver.findElement(By.id("c411PeopleReverseFind"));
+   		   element.click();
+   		 driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
+   		 
+   
+   		 
+   		 element = driver.findElement(By.xpath("//*[@id=\"ypgBody\"]/div[2]/div[1]/div[3]/div/div/div[1]/h1/span"));
+   		 if(element!=null)
+   		 {
+   			 System.out.println(element.getText());
+   		 }
+   		 
+   		 int count=1;
+   while(count<4)
+   {
+   		element = driver.findElement(By.xpath("//*[@id=\"ypgBody\"]/div[2]/div[2]/div/div/div[1]/div[1]/div[2]/div/span["+count+"]"));
+   	 if(element!=null)
+		 {
+			 System.out.println(element.getText());
+		 }
+   	 count++;
+   } 
+   		 //element=driver.findElement(By.cssSelector("close newsletter-modal__close"));
+   		 //element.click();
+   		 
+   		  // driver.navigate().to("http://www.canada411.ca/");
+   	   //}
+   	   driver.close();
+	   
+   	
 	}
 
 }
